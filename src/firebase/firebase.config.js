eb56e195900d2fbdc8701cad39b9d580aedb89bd
abutalha-97 <abutalhapappu@gmail.com>
2021-10-19 23:58:24 +0600

    final

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBDgto3JIwxsZkFFipWy9pZGJEXZRi8u0A",
-    authDomain: "talhamedicare-2d4d2.firebaseapp.com",
-    projectId: "talhamedicare-2d4d2",
-    storageBucket: "talhamedicare-2d4d2.appspot.com",
-    messagingSenderId: "168684709662",
-    appId: "1:168684709662:web:e93090a0530d1b5f46cb0f"
+    apiKey: "AIzaSyCT1oSwp6z51Q1E1Ir5t9-WYfGqE_HOj0s",
+    authDomain: "talhamedicare.firebaseapp.com",
+    projectId: "talhamedicare",
+    storageBucket: "talhamedicare.appspot.com",
+    messagingSenderId: "837991188047",
+    appId: "1:837991188047:web:ee2246be04b238bfe7cc3a"
 };
 export default firebaseConfig;
